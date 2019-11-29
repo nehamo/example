262417f0629b9a5c248372bdd596c65e52002292
nehamohan <nehamohan1018@gmail.com>
2019-11-29 10:55:45 +0530

    I added mu opinion

--- hungry.py
+++ hungry.py
@@ -3,4 +3,10 @@ if hungry=="yes":
     print("eat chocolate")
 else
     print("study lessons")
+ 
+ 
+ 
+print("Are you going to college today?");
+print("This is the last day of ur college"); 
+ 
     
